354b1b31028988d9a3d63a27464478c0b62ba5c2
Robson Vieira <robson.ppmg@gmail.com>
2021-12-27 19:25:04 -0300

    primeiro commit

---
+++ src/main/java/com/tqi/evolution/backend/project_tqi/ProjectTqiApplication.java
@@ -0,0 +1,13 @@
+package com.tqi.evolution.backend.project_tqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tqi/evolution/backend/project_tqi/ProjectTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqi.evolution.backend.project_tqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
